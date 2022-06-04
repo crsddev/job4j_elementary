3bc30e8c3a887ce7776144556f41447f49bc7436
CursedAndBlessed <8udzumyki@gmail.com>
2022-06-04 13:21:20 +0300

    4.2. Тернарное сравнение [#189] - доработка

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,9 +2,6 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        if (left == right) {
-            return right;
-        }
-        return (left > right) ? left : right;
+        return (left >= right) ? left : right;
     }
 }
