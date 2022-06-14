91058077f36e7abb2bec9a3e8cfe35b63a134efe
CursedAndBlessed <8udzumyki@gmail.com>
2022-06-15 00:12:22 +0300

    5.4. Протеиновая диета [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (nik >= ivan) {
+            if (ivan > nik) {
+                break;
+            }
+            month++;
+            nik *= 2;
+            ivan *= 3;
+        }
+        return month;
+    }
+}
