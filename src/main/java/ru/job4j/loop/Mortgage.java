564a8f5a6d7aae5eaa41fed12ee1fbdeabe65dde
CursedAndBlessed <8udzumyki@gmail.com>
2022-06-15 18:45:00 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount >= 0) {
+            amount += amount * (percent / 100);
+            amount -= salary;
+            year++;
+            if (amount <= 0) {
+                break;
+            }
+        }
+        return year;
+    }
+}
