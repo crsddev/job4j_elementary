7c32468ccebfb36d30b683742f7cbb462c66d90f
crsd <againnft@gmail.com>
2023-09-26 17:06:37 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,26 +1,25 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
+
+    public static final String NEW_VALUE = "";
     private int sizeOfEmpty = 10;
 
     public String surname;
 
-    public static final String NEWVALUE = "";
+    String name;
 
     void echo() {
     }
 
     void media(Object obj) {
         if (obj != null) {
-                System.out.println(obj);
+            System.out.println(obj);
         }
     }
 
     void method(int a, int b, int c, int d, int e, int f, int g) {
-
     }
-
-    String name;
 
     Broken() {
     }
